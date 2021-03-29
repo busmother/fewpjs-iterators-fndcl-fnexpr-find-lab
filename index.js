4b38e7bba246d704848928d0814e0d6189fb3f37
Ian Marshall <imarshall14@gmail.com>
2021-03-29 18:42:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array) {
+    let winner = array.find(function(s) {return s["result"] === "W"})
+    if (winner === undefined) {
+        return undefined
+    } else {
+        return winner["year"]
+    }
+    
+}
